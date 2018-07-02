185500aac5706b474efcf803096194ea743f278d
subrahmanyam.o <subrahmanyam.o@SON16095.SONATA.LOCAL>
2018-07-02 13:42:24 +0530

    Maven With PMD

---
+++ src/main/java/com/pmd/test/CopyTest.java
@@ -0,0 +1,12 @@
+package com.pmd.test;
+
+public class CopyTest {
+public static void main(String args[]) {
+	try {
+		System.out.println("Hi...");
+	}catch(Exception ex) {
+		
+	}
+
+}
+}
---
+++ src/main/java/com/pmd/test/PmdTest.java
@@ -0,0 +1,13 @@
+package com.pmd.test;
+
+public class PmdTest {
+	private static String name;
+public static void main(String args[]) {
+	try {
+		System.out.println("Hi..."+name);
+		System.out.println();
+	}catch(Exception ex) {
+
+	}
+}
+}
